896e4b984f4344f4d9803d42bdfac7b3ab93b10e
brandtryan <emailme@brandtryan.com>
2025-03-30 07:53:12 -0400

    Initialize

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  useTabs: true,
+  arrowParens: "avoid",
+  semi: true,
+  singleQuote: true,
+};
+
+export default config;
